ea3395c67121bf82ee1b3a2be6614697cc92db09
a-cs <acarneirosousa@gmail.com>
2025-12-09 19:07:12 -0300

    creating tasks summary

--- todo/src/pages/page-home.tsx
+++ todo/src/pages/page-home.tsx
@@ -1,5 +1,10 @@
 import Container from "../components/container";
+import TasksSumary from "../core-components/tasks-summary";
 
 export default function PageHome() {
-	return <Container>Home</Container>
+	return <Container as="article" className="space-y-3">
+		<header className="flex items-center justify-between">
+			<TasksSumary/>
+		</header>
+	</Container>
 }

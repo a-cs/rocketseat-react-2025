8f1ffcbeebd116ceb2e266a42bf598b2aa4a316e
a-cs <acarneirosousa@gmail.com>
2025-12-10 19:11:39 -0300

    listing tasks

--- todo/src/core-components/task-item.tsx
+++ todo/src/core-components/task-item.tsx
@@ -8,9 +8,15 @@ import PencilIcon from "../assets/icons/pencil.svg?react"
 import XIcon from "../assets/icons/x.svg?react"
 import { useState } from "react";
 import InputText from "../components/input-text";
+import { TaskState, type Task } from "../models/task";
+import { cx } from "class-variance-authority";
 
-export default function TaskItem() {
-	const [isEditing, setIsEditing] = useState(false)
+interface TaskItemProps {
+	task: Task
+}
+
+export default function TaskItem({ task }: TaskItemProps) {
+	const [isEditing, setIsEditing] = useState(task?.state === TaskState.Creating)
 
 	function handleEditTask() {
 		setIsEditing(true)
@@ -24,8 +30,10 @@ export default function TaskItem() {
 		<Card size="md" className="flex items-center gap-4">
 			{!isEditing ? (
 				<>
-					<InputCheckbox />
-					<Text className="flex-1">📦Fazer compras da semanae</Text>
+					<InputCheckbox value={task?.concluded?.toString()} checked={task?.concluded}/>
+					<Text className={cx("flex-1", {
+						"line-through": task.concluded
+					})}>{task?.title}</Text>
 					<div className="flex gap-1">
 						<ButtonIcon icon={TrashIcon} variant="tertiary" />
 						<ButtonIcon icon={PencilIcon} variant="tertiary" onClick={handleEditTask} />

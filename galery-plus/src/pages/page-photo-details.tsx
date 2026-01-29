2565b5079ddbd23dc5063d792ba2803afa675e1c
a-cs <acarneirosousa@gmail.com>
2026-01-28 21:51:13 -0300

    photo navigator component

--- galery-plus/src/pages/page-photo-details.tsx
+++ galery-plus/src/pages/page-photo-details.tsx
@@ -1,14 +1,35 @@
 import { useParams } from "react-router";
 import Text from "../components/text";
+import Container from "../components/container";
+import type { Photo } from "../contexts/photos/models/photo";
+import Skeleton from "../components/skeleton";
+import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 
 export default function PagePhotoDetails() {
 	const { id } = useParams()
+	//apenas para o mock
+	const isLoadingPhoto = false
+	const photo = {
+		id: "123",
+		title: "Olá mundo",
+		imageId: "portrait-tower.png",
+		album: [
+			{ id: "3421", title: "Album 1" },
+			{ id: "123", title: "Album 2" },
+			{ id: "456", title: "Album 3" },
+		]
+	} as Photo
 
 	return (
-		<>
-			<Text variant="heading-medium">Photo Details</Text>
-			<hr />
-			<Text variant="heading-medium">ID: {id}</Text>
-		</>
+		<Container>
+			<header className="flex items-center justify-between gap-8 mb-8">
+				{!isLoadingPhoto ? (
+					<Text variant="heading-large">{photo.title}</Text>
+				) : (
+					<Skeleton className="w-48 h-8" />
+				)}
+				<PhotosNavigator loading={isLoadingPhoto} />
+			</header>
+		</Container>
 	)
 }

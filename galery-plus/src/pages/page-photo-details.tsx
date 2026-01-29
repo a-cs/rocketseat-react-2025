c6a3a2f327e60284ecebb6e7288da7d4f7185855
a-cs <acarneirosousa@gmail.com>
2026-01-28 22:00:37 -0300

    update photo details component

--- galery-plus/src/pages/page-photo-details.tsx
+++ galery-plus/src/pages/page-photo-details.tsx
@@ -4,6 +4,8 @@ import Container from "../components/container";
 import type { Photo } from "../contexts/photos/models/photo";
 import Skeleton from "../components/skeleton";
 import PhotosNavigator from "../contexts/photos/components/photos-navigator";
+import ImagePreview from "../components/image-preview";
+import Button from "../components/button";
 
 export default function PagePhotoDetails() {
 	const { id } = useParams()
@@ -30,6 +32,30 @@ export default function PagePhotoDetails() {
 				)}
 				<PhotosNavigator loading={isLoadingPhoto} />
 			</header>
+			<div className="grid grid-cols-[21rem] gap-24">
+				<div className="space-y-3">
+					{!isLoadingPhoto ?
+						<div className="flex flex-col gap-3">
+							<ImagePreview
+								src={`/images/${photo?.imageId}`}
+								title={photo.title}
+								imageClassName="h-[21rem]"
+							/>
+						</div> :
+						(
+							<Skeleton className="h-[21rem]" />
+						)}
+
+					{!isLoadingPhoto ?
+						<Button variant="destructive">
+							Excluir
+						</Button>
+						:
+					(
+						<Skeleton className="w-20 h-10"/>
+					)}
+				</div>
+			</div>
 		</Container>
 	)
 }

b0cb147320fa1479bc8c38f819375008a9976f88
a-cs <acarneirosousa@gmail.com>
2026-01-31 16:57:47 -0300

    albums list selectable component

--- galery-plus/src/pages/page-photo-details.tsx
+++ galery-plus/src/pages/page-photo-details.tsx
@@ -6,6 +6,7 @@ import Skeleton from "../components/skeleton";
 import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 import ImagePreview from "../components/image-preview";
 import Button from "../components/button";
+import AlbumsListSelectable from "../contexts/album/components/albums-list-selectable";
 
 export default function PagePhotoDetails() {
 	const { id } = useParams()
@@ -15,7 +16,7 @@ export default function PagePhotoDetails() {
 		id: "123",
 		title: "Olá mundo",
 		imageId: "portrait-tower.png",
-		album: [
+		albums: [
 			{ id: "3421", title: "Album 1" },
 			{ id: "123", title: "Album 2" },
 			{ id: "456", title: "Album 3" },
@@ -26,13 +27,13 @@ export default function PagePhotoDetails() {
 		<Container>
 			<header className="flex items-center justify-between gap-8 mb-8">
 				{!isLoadingPhoto ? (
-					<Text variant="heading-large">{photo.title}</Text>
+					<Text as="h2" variant="heading-large">{photo.title}</Text>
 				) : (
 					<Skeleton className="w-48 h-8" />
 				)}
 				<PhotosNavigator loading={isLoadingPhoto} />
 			</header>
-			<div className="grid grid-cols-[21rem] gap-24">
+			<div className="grid grid-cols-[21rem_1fr] gap-24">
 				<div className="space-y-3">
 					{!isLoadingPhoto ?
 						<div className="flex flex-col gap-3">
@@ -51,9 +52,24 @@ export default function PagePhotoDetails() {
 							Excluir
 						</Button>
 						:
-					(
-						<Skeleton className="w-20 h-10"/>
-					)}
+						(
+							<Skeleton className="w-20 h-10" />
+						)}
+				</div>
+
+				<div className="py-3">
+					<Text as="h3" variant="heading-medium" className="mb-6">
+						Álbuns
+					</Text>
+					<AlbumsListSelectable
+						photo={photo}
+						albums={[
+							{ id: "3421", title: "Album 1" },
+							{ id: "123", title: "Album 2" },
+							{ id: "456", title: "Album 3" },
+						]}
+						loading={isLoadingPhoto}
+					/>
 				</div>
 			</div>
 		</Container>

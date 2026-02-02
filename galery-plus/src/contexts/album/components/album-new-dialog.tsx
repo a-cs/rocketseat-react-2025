78749d5df8361c239c3b158ca956f6b69288e5b8
a-cs <acarneirosousa@gmail.com>
2026-02-01 22:03:01 -0300

    list photos filter

--- galery-plus/src/contexts/album/components/album-new-dialog.tsx
+++ galery-plus/src/contexts/album/components/album-new-dialog.tsx
@@ -3,7 +3,6 @@ import { Dialog, DialogBody, DialogClose, DialogContent, DialogFooter, DialogHea
 import Button from "../../../components/button"
 import InputText from "../../../components/input-text"
 import Text from "../../../components/text"
-import type { Photo } from "../../photos/models/photo"
 import SelectCheckboxIlustration from "../../../assets/images/select-checkbox.svg?react"
 import Skeleton from "../../../components/skeleton"
 import PhotoImageSelectable from "../../photos/components/photo-image-selectable"

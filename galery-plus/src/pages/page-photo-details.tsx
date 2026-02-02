9645af2abea5c0b7e15ce917c1eb2dd8b25d794f
a-cs <acarneirosousa@gmail.com>
2026-02-01 22:21:46 -0300

    photos details query

--- galery-plus/src/pages/page-photo-details.tsx
+++ galery-plus/src/pages/page-photo-details.tsx
@@ -1,47 +1,45 @@
 import { useParams } from "react-router";
 import Text from "../components/text";
 import Container from "../components/container";
-import type { Photo } from "../contexts/photos/models/photo";
 import Skeleton from "../components/skeleton";
 import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 import ImagePreview from "../components/image-preview";
 import Button from "../components/button";
 import AlbumsListSelectable from "../contexts/album/components/albums-list-selectable";
 import useAlbums from "../contexts/album/hooks/use-albums";
+import usePhoto from "../contexts/photos/hooks/use-photo";
+import type { Photo } from "../contexts/photos/models/photo";
 
 export default function PagePhotoDetails() {
 	const { id } = useParams()
-	const {albums, isLoadingAlbums} = useAlbums()
-	//apenas para o mock
-	const isLoadingPhoto = false
-	const photo = {
-		id: "123",
-		title: "Olá mundo",
-		imageId: "portrait-tower.png",
-		albums: [
-			{ id: "3421", title: "Album 1" },
-			{ id: "123", title: "Album 2" },
-			{ id: "456", title: "Album 3" },
-		]
-	} as Photo
+	const { photo, nextPhotoId, previousPhotoId, isLoadingPhoto } = usePhoto(id)
+	const { albums, isLoadingAlbums } = useAlbums()
+
+	if (!isLoadingPhoto && !photo) {
+		return <div>Foto não encontrada</div>
+	}
 
 	return (
 		<Container>
 			<header className="flex items-center justify-between gap-8 mb-8">
 				{!isLoadingPhoto ? (
-					<Text as="h2" variant="heading-large">{photo.title}</Text>
+					<Text as="h2" variant="heading-large">{photo?.title}</Text>
 				) : (
 					<Skeleton className="w-48 h-8" />
 				)}
-				<PhotosNavigator loading={isLoadingPhoto} />
+				<PhotosNavigator
+					nextPhotoId={nextPhotoId}
+					previousPhotoId={previousPhotoId}
+					loading={isLoadingPhoto}
+				/>
 			</header>
 			<div className="grid grid-cols-[21rem_1fr] gap-24">
 				<div className="space-y-3">
 					{!isLoadingPhoto ?
 						<div className="flex flex-col gap-3">
 							<ImagePreview
-								src={`/images/${photo?.imageId}`}
-								title={photo.title}
+								src={`${import.meta.env.VITE_IMAGES_URL}/${photo?.imageId}`}
+								title={photo?.title}
 								imageClassName="h-[21rem]"
 							/>
 						</div> :
@@ -64,7 +62,7 @@ export default function PagePhotoDetails() {
 						Álbuns
 					</Text>
 					<AlbumsListSelectable
-						photo={photo}
+						photo={photo as Photo}
 						albums={albums}
 						loading={isLoadingAlbums}
 					/>

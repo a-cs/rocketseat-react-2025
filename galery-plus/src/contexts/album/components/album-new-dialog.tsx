12805c4a780c81fda628f6f85380ce0ebeeb566f
a-cs <acarneirosousa@gmail.com>
2026-02-01 21:19:02 -0300

    api helper

--- galery-plus/src/contexts/album/components/album-new-dialog.tsx
+++ galery-plus/src/contexts/album/components/album-new-dialog.tsx
@@ -6,7 +6,6 @@ import Text from "../../../components/text"
 import type { Photo } from "../../photos/models/photo"
 import SelectCheckboxIlustration from "../../../assets/images/select-checkbox.svg?react"
 import Skeleton from "../../../components/skeleton"
-import ImagePreview from "../../../components/image-preview"
 import PhotoImageSelectable from "../../photos/components/photo-image-selectable"
 
 interface AlbumNewDialog {
